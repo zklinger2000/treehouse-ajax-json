2a4704c945c9a509f83c28e698cbdee0f8af1eb9
Zack Klinger <zklinger@gmail.com>
2015-05-30 13:45:35 -0700

    ADD another AJAX call

--- js/widget.js
+++ js/widget.js
@@ -28,3 +28,35 @@ xhr.onreadystatechange = function () {
 xhr.open('GET', 'data/employees.json');
 //send the Request to the server
 xhr.send();
+
+//AJAX Request object
+var roomRequest = new XMLHttpRequest();
+//setting up state handler
+//this is the function's callback
+roomRequest.onreadystatechange = function () {
+  //readyState '4' is when browser is done getting response
+  if (roomRequest.readyState === 4) {
+    //getting the list of rooms into an array
+    var rooms = JSON.parse(roomRequest.responseText);
+    //creating an <ul> element to store the list
+    var statusHTML = '<ul class="bulleted">';
+    //looping through the employee array
+    for (var i = 0, len = rooms.length; i < len; ++i) {
+      if (rooms[i].inoffice === true) {
+        //setting class of each <li> to 'in' or 'out'
+        statusHTML += '<li class="in">';
+      } else {
+        statusHTML += '<li class="out">';
+      }
+      statusHTML += rooms[i].room;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('employeeList').innerHTML = statusHTML;
+  }
+};
+//Opening the AJAX Request objest and load a file filled with JSON data
+roomRequest.open('GET', 'data/rooms.json');
+//send the Request to the server
+roomRequest.send();
+
